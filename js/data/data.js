d204aa3eba0c6fd3b9e7fd1754820ca918be535f
pvmoran <47060960+pvmoran@users.noreply.github.com>
2019-04-15 17:59:00 -0500

    Add files via upload

---
+++ js/data/data.js
@@ -0,0 +1,19 @@
+showEvents({
+  "events": [
+     {
+       "location": "San Francisco, CA",
+       "date": "May 1",
+       "map": "http://javascriptbook.com/code/c08/img/map-ca.png"
+     },
+     {
+       "location": "Austin, TX",
+       "date": "May 15",
+       "map": "http://javascriptbook.com/code/c08/img/map-tx.png"
+     },
+     {
+       "location": "New York, NY",
+       "date": "May 30",
+       "map": "http://javascriptbook.com/code/c08/img/map-ny.png"
+     }
+   ]
+});
